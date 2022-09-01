38fed96bbd303fa884edf5d9168c2038653c8a2b
Bogdan <zykovbogdan07@gmail.com>
2022-09-01 17:22:38 +0300

    Initial Commit

---
+++ Viper SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Viper SwiftUI
+//
+//  Created by Богдан Зыков on 01.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Viper SwiftUI/Viper_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Viper_SwiftUIApp.swift
+//  Viper SwiftUI
+//
+//  Created by Богдан Зыков on 01.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Viper_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
